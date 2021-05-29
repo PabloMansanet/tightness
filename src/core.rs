76d4ab1333d842df63f7fee71379069348cd5ba1
PabloMansanet <pablomansanet@gmail.com>
2021-05-30 00:34:28 +0200

    WIP blanket implementations

--- src/core.rs
+++ src/core.rs
@@ -1,19 +1,18 @@
-#![forbid(unsafe_code)]
-use std::{marker::PhantomData, ops::Deref};
+use std::{borrow::Borrow, marker::PhantomData, ops::Deref};
 
+use thiserror::Error;
 
-#[derive(Debug)]
-pub struct Error;
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Invariant broken on construction")]
+    ConstructionFailed,
+    #[error("Invariant broken on mutation")]
+    MutationFailed
+}
 
 pub trait Bound {
     type Target;
     fn check(target: &Self::Target) -> bool;
-}
-
-pub trait ExpressiveBound {
-    type Target;
-    type Error;
-    fn check(target: &Self::Target) -> Result<(), Self::Error> ;
 }
 
 #[derive(Debug)]
@@ -22,43 +21,9 @@ pub struct Bounded<T, B: Bound<Target=T>> {
     _marker: PhantomData<B>
 }
 
-#[derive(Debug)]
-pub struct ExpressiveBounded<T, B: ExpressiveBound<Target=T>> {
-    inner: T,
-    _marker: PhantomData<B>
-}
-
-impl<T, B: ExpressiveBound<Target=T>> ExpressiveBounded<T, B> {
-    pub fn new(t: T) -> Result<Self, B::Error> {
-        match B::check(&t) {
-            Ok(_) => Ok(Self{ inner: t, _marker: Default::default() }),
-            Err(e) => Err(e),
-        }
-    }
-
-    /// Preserves invariants after mutation. Panics if invariants are not upheld
-    pub fn mutate(&mut self, f: impl FnOnce(&mut T)) {
-        f(&mut self.inner);
-        assert!(B::check(&self.inner).is_ok());
-    }
-
-    /// If invariants aren't preserved after mutation, restores to a given value
-    pub fn mutate_or(&mut self, f: impl FnOnce(&mut T), default: Self) -> Result<(), B::Error> {
-        f(&mut self.inner);
-
-        match B::check(&self.inner) {
-            Ok(_) => Ok(()),
-            Err(e) => {
-                *self = default;
-                Err(e)
-            }
-        }
-    }
-}
-
 impl<T, B: Bound<Target=T>> Bounded<T, B> {
     pub fn new(t: T) -> Result<Self, Error> {
-        if B::check(&t) { Ok(Self{ inner: t, _marker: Default::default() }) } else { Err(Error) }
+        if B::check(&t) { Ok(Self{ inner: t, _marker: Default::default() }) } else { Err(Error::ConstructionFailed) }
     }
 
     /// Preserves invariants after mutation. Panics if invariants are not upheld
@@ -70,7 +35,20 @@ impl<T, B: Bound<Target=T>> Bounded<T, B> {
     /// If invariants aren't preserved after mutation, restores to a given value
     pub fn mutate_or(&mut self, f: impl FnOnce(&mut T), default: Self) -> Result<(), Error> {
         f(&mut self.inner);
-        if B::check(&self.inner) { Ok(()) } else { *self = default; Err(Error) }
+        if B::check(&self.inner) { Ok(()) } else {
+            *self = default;
+            Err(Error::MutationFailed)
+        }
+    }
+
+    /// Invariant must be upheld manually!
+    pub unsafe fn new_unchecked(t: T) -> Self {
+        Self { inner: t, _marker: Default::default() }
+    }
+
+    /// Invariant must be upheld manually!
+    pub unsafe fn mutate_unchecked(&mut self, f: impl FnOnce(&mut T)){
+        f(&mut self.inner)
     }
 }
 
@@ -84,9 +62,19 @@ impl<T: Clone, B: Bound<Target=T>> Bounded<T, B> {
             self.inner = duplicate;
             Ok(())
         } else {
-            Err(Error)
+            Err(Error::MutationFailed)
         }
     }
+}
+
+impl<T: Clone, B: Bound<Target=T>> Clone for Bounded<T, B> {
+    fn clone(&self) -> Self {
+        Self { inner: self.inner.clone(), _marker: Default::default()  }
+    }
+}
+
+impl<T, B: Bound<Target=T>> Borrow<T> for Bounded<T, B> {
+    fn borrow(&self) -> &T { &self.inner }
 }
 
 impl<T, B: Bound<Target=T>> Deref for Bounded<T, B> {
@@ -94,10 +82,8 @@ impl<T, B: Bound<Target=T>> Deref for Bounded<T, B> {
     fn deref(&self) -> &Self::Target { &self.inner }
 }
 
-
 #[cfg(test)]
 mod tests {
-
     #[derive(Debug)]
     struct IsPositive;
     impl Bound for IsPositive {
@@ -105,28 +91,15 @@ mod tests {
         fn check(x: &i32) -> bool { *x >= 0 }
     }
 
-    #[derive(Debug)]
-    struct IsPositiveExpressive;
-    impl ExpressiveBound for IsPositiveExpressive {
-        type Target = i32;
-        type Error = ();
-        fn check(x: &i32) -> Result<(), Self::Error> {
-            if *x >= 0 { Ok(()) } else { Err(())}
-        }
-
-    }
-
     use super::*;
     #[test]
     fn constructing_with_passing_bounds_succeeds() {
        Bounded::<i32, IsPositive>::new(1).unwrap();
-       ExpressiveBounded::<i32, IsPositiveExpressive>::new(1).unwrap();
     }
 
     #[test]
     fn constructing_with_failing_bounds_fails() {
        assert!(Bounded::<i32, IsPositive>::new(-5).is_err());
-       assert!(matches!(ExpressiveBounded::<i32, IsPositiveExpressive>::new(-1), Err(_)));
     }
 
     #[test]
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,69 @@
-pub use tightness_core::*;
-pub use tightness_proc::bound;
+#[forbid(unsafe_code)]
+pub use paste::paste;
+pub use crate::core::*;
+
+
+mod core;
+mod impls;
+
+#[macro_export]
+macro_rules! bound {
+    ($name:ident: $type:ty where $check:expr) => { $crate::paste! {
+        #[derive(Debug)]
+        pub struct [<$name Bound>];
+
+        impl $crate::Bound for [<$name Bound>] {
+            type Target = $type;
+            fn check(target: &Self::Target) -> bool {
+                let check: fn(&Self::Target) -> bool = $check;
+                check(target)
+            }
+        }
+
+        pub type $name = Bounded<$type, [<$name Bound>]>;
+    }}
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    bound!(Password: String where |p| p.len() < 8 && p.chars().all(char::is_alphanumeric));
+    bound!(Month: usize where |m| *m < 12usize);
+
+    impl std::ops::Add<usize> for Month {
+        type Output = Self;
+
+        fn add(mut self, rhs: usize) -> Self::Output {
+            self.mutate(|m| *m = (*m + rhs) & 12usize);
+            self
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_bound_string_operation_panics() {
+        let mut password = Password::new("Hello".to_owned()).unwrap();
+        password.mutate(|p| p.push_str("World"));
+    }
+
+    #[test]
+    fn fallible_constructions_fail_on_invalid_input() {
+        assert!(Month::new(22).is_err());
+        assert!(Password::new("---".to_owned()).is_err());
+    }
+
+    #[test]
+    fn fallible_mutations_fail_on_invalid_final_values() {
+        let mut month = Month::new(7).unwrap();
+        let impossible_mutation = |m: &mut usize| *m = *m + 13;
+        assert!(matches!(month.try_mutate(impossible_mutation), Err(Error::MutationFailed)));
+        assert!(month.mutate_or(impossible_mutation, month.clone()).is_err());
+    }
+
+    #[test]
+    fn convenient_operators_on_bounded_types() {
+        let password = Password::new("Hello".to_owned()).unwrap();
+        assert_eq!(password, "Hello");
+    }
+}
