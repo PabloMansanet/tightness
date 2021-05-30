9bac04648f3f283e328e6c1483909da472ff52bf
PabloMansanet <pablomansanet@gmail.com>
2021-05-30 10:52:24 +0200

    Add meaningful blanket implementations

--- src/core.rs
+++ src/core.rs
@@ -1,4 +1,4 @@
-use std::{borrow::Borrow, marker::PhantomData, ops::Deref};
+use std::{borrow::Borrow, marker::PhantomData, ops::{Deref, Index}};
 
 use thiserror::Error;
 
@@ -7,7 +7,9 @@ pub enum Error {
     #[error("Invariant broken on construction")]
     ConstructionFailed,
     #[error("Invariant broken on mutation")]
-    MutationFailed
+    MutationFailed,
+    #[error("Invariant broken at an unspecified point")]
+    BrokenInvariant,
 }
 
 pub trait Bound {
@@ -16,50 +18,73 @@ pub trait Bound {
 }
 
 #[derive(Debug)]
-pub struct Bounded<T, B: Bound<Target=T>> {
-    inner: T,
-    _marker: PhantomData<B>
-}
+pub struct Bounded<T, B: Bound<Target = T>>(T, PhantomData<B>);
 
-impl<T, B: Bound<Target=T>> Bounded<T, B> {
+impl<T, B: Bound<Target = T>> Bounded<T, B> {
     pub fn new(t: T) -> Result<Self, Error> {
-        if B::check(&t) { Ok(Self{ inner: t, _marker: Default::default() }) } else { Err(Error::ConstructionFailed) }
+        if B::check(&t) {
+            Ok(Self(t, Default::default()))
+        } else {
+            Err(Error::ConstructionFailed)
+        }
     }
 
     /// Preserves invariants after mutation. Panics if invariants are not upheld
     pub fn mutate(&mut self, f: impl FnOnce(&mut T)) {
-        f(&mut self.inner);
-        assert!(B::check(&self.inner));
+        f(&mut self.0);
+        assert!(B::check(&self.0));
     }
 
     /// If invariants aren't preserved after mutation, restores to a given value
     pub fn mutate_or(&mut self, f: impl FnOnce(&mut T), default: Self) -> Result<(), Error> {
-        f(&mut self.inner);
-        if B::check(&self.inner) { Ok(()) } else {
+        f(&mut self.0);
+        if B::check(&self.0) {
+            Ok(())
+        } else {
             *self = default;
             Err(Error::MutationFailed)
         }
     }
 
-    /// Invariant must be upheld manually!
-    pub unsafe fn new_unchecked(t: T) -> Self {
-        Self { inner: t, _marker: Default::default() }
+    /// Inner value is dropped if mutation fails
+    pub fn into_mutated(mut self, f: impl FnOnce(&mut T)) -> Result<Self, Error> {
+        f(&mut self.0);
+        if B::check(&self.0) {
+            Ok(self)
+        } else {
+            Err(Error::MutationFailed)
+        }
     }
 
     /// Invariant must be upheld manually!
-    pub unsafe fn mutate_unchecked(&mut self, f: impl FnOnce(&mut T)){
-        f(&mut self.inner)
+    pub unsafe fn new_unchecked(t: T) -> Self { Self(t, Default::default()) }
+
+    /// Invariant must be upheld manually!
+    pub unsafe fn mutate_unchecked(&mut self, f: impl FnOnce(&mut T)) { f(&mut self.0) }
+
+    /// Gives mutable access to the internals without upholding invariants.
+    /// They must continue to be upheld manually while the reference lives!
+    pub unsafe fn get_mut(&mut self) -> &mut T { &mut self.0 }
+
+    /// Verifies invariants. This is guaranteed to succeed unless you've used
+    /// one of the `unsafe` methods that require variants to be manually upheld
+    pub fn verify(&self) -> Result<(), Error> {
+        if B::check(&self.0) {
+            Ok(())
+        } else {
+            Err(Error::BrokenInvariant)
+        }
     }
 }
 
-impl<T: Clone, B: Bound<Target=T>> Bounded<T, B> {
+impl<T: Clone, B: Bound<Target = T>> Bounded<T, B> {
     /// Preserves invariants after mutation, erroring out if the mutation broke
     /// the invariants. Requires a copy to ensure the actual value is recoverable
     pub fn try_mutate(&mut self, f: impl FnOnce(&mut T)) -> Result<(), Error> {
-        let mut duplicate = self.inner.clone();
+        let mut duplicate = self.0.clone();
         f(&mut duplicate);
         if B::check(&duplicate) {
-            self.inner = duplicate;
+            self.0 = duplicate;
             Ok(())
         } else {
             Err(Error::MutationFailed)
@@ -67,20 +92,47 @@ impl<T: Clone, B: Bound<Target=T>> Bounded<T, B> {
     }
 }
 
-impl<T: Clone, B: Bound<Target=T>> Clone for Bounded<T, B> {
-    fn clone(&self) -> Self {
-        Self { inner: self.inner.clone(), _marker: Default::default()  }
+impl<T: Clone, B: Bound<Target = T>> Clone for Bounded<T, B> {
+    fn clone(&self) -> Self { Self(self.0.clone(), Default::default()) }
+}
+
+impl<T, B: Bound<Target = T>> Borrow<T> for Bounded<T, B> {
+    fn borrow(&self) -> &T { &self.0 }
+}
+
+impl<T, B: Bound<Target = T>> AsRef<T> for Bounded<T, B> {
+    fn as_ref(&self) -> &T { &self.0 }
+}
+
+impl<T, B: Bound<Target = T>> Deref for Bounded<T, B> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target { &self.0 }
+}
+
+impl<T: PartialEq, B: Bound<Target = T>> PartialEq for Bounded<T, B> {
+    fn eq(&self, other: &Self) -> bool { self.0.eq(&other.0) }
+}
+
+impl<T: Eq, B: Bound<Target = T>> Eq for Bounded<T, B> {}
+
+impl<T: PartialOrd, B: Bound<Target = T>> PartialOrd for Bounded<T, B> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.0.partial_cmp(&other.0)
     }
 }
 
-impl<T, B: Bound<Target=T>> Borrow<T> for Bounded<T, B> {
-    fn borrow(&self) -> &T { &self.inner }
+impl<T: Ord, B: Bound<Target = T>> Ord for Bounded<T, B> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.cmp(&other.0)
+    }
 }
 
-impl<T, B: Bound<Target=T>> Deref for Bounded<T, B> {
-    type Target = T;
-    fn deref(&self) -> &Self::Target { &self.inner }
+impl<T: Index<U>, U, B: Bound<Target = T>> Index<U> for Bounded<T, B> {
+    type Output = T::Output;
+
+    fn index(&self, index: U) -> &Self::Output { self.0.index(index) }
 }
+
 
 #[cfg(test)]
 mod tests {
@@ -93,32 +145,30 @@ mod tests {
 
     use super::*;
     #[test]
-    fn constructing_with_passing_bounds_succeeds() {
-       Bounded::<i32, IsPositive>::new(1).unwrap();
-    }
+    fn constructing_with_passing_bounds_succeeds() { Bounded::<i32, IsPositive>::new(1).unwrap(); }
 
     #[test]
     fn constructing_with_failing_bounds_fails() {
-       assert!(Bounded::<i32, IsPositive>::new(-5).is_err());
+        assert!(Bounded::<i32, IsPositive>::new(-5).is_err());
     }
 
     #[test]
     fn mutating_with_passing_bounds_succeeds() {
-       let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
-       bounded.mutate(|i| *i = 2 * *i);
+        let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
+        bounded.mutate(|i| *i = 2 * *i);
     }
 
     #[test]
     #[should_panic]
     fn mutating_with_failing_bounds_panics() {
-       let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
-       bounded.mutate(|i| *i = -5);
+        let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
+        bounded.mutate(|i| *i = -5);
     }
 
     #[test]
     #[should_panic]
     fn expressive_mutating_with_failing_bounds_panics() {
-       let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
-       bounded.mutate(|i| *i = -5);
+        let mut bounded = Bounded::<i32, IsPositive>::new(5i32).unwrap();
+        bounded.mutate(|i| *i = -5);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,7 @@
 pub use paste::paste;
 pub use crate::core::*;
 
-
 mod core;
-mod impls;
 
 #[macro_export]
 macro_rules! bound {
@@ -24,12 +22,12 @@ macro_rules! bound {
     }}
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
     bound!(Password: String where |p| p.len() < 8 && p.chars().all(char::is_alphanumeric));
     bound!(Month: usize where |m| *m < 12usize);
+    bound!(XorPair: (bool, bool) where |(a, b)| a ^ b);
 
     impl std::ops::Add<usize> for Month {
         type Output = Self;
@@ -51,6 +49,7 @@ mod tests {
     fn fallible_constructions_fail_on_invalid_input() {
         assert!(Month::new(22).is_err());
         assert!(Password::new("---".to_owned()).is_err());
+        assert!(XorPair::new((true, true)).is_err());
     }
 
     #[test]
@@ -58,12 +57,33 @@ mod tests {
         let mut month = Month::new(7).unwrap();
         let impossible_mutation = |m: &mut usize| *m = *m + 13;
         assert!(matches!(month.try_mutate(impossible_mutation), Err(Error::MutationFailed)));
-        assert!(month.mutate_or(impossible_mutation, month.clone()).is_err());
+        assert!(matches!(month.mutate_or(impossible_mutation, month.clone()), Err(Error::MutationFailed)));
+        assert!(matches!(month.into_mutated(impossible_mutation), Err(Error::MutationFailed)));
+
+        let mut xor_pair = XorPair::new((true, false)).unwrap();
+        assert!(matches!(xor_pair.try_mutate(|(a, b)| *a = *b), Err(Error::MutationFailed)));
+    }
+
+    #[test]
+    fn fallible_mutations_succeed_on_valid_final_values() {
+        let mut month = Month::new(7).unwrap();
+        month.try_mutate(|m| *m += 1).unwrap();
+        assert_eq!(*month, 8);
+        month.mutate_or(|m| *m += 1, month.clone()).unwrap();
+        assert_eq!(*month, 9);
+        let month = month.into_mutated(|m| *m += 1).unwrap();
+        assert_eq!(*month, 10);
     }
 
     #[test]
     fn convenient_operators_on_bounded_types() {
-        let password = Password::new("Hello".to_owned()).unwrap();
-        assert_eq!(password, "Hello");
+        fn takes_as_ref<T: AsRef<usize>>(_: &T) {}
+        let month = Month::new(1).unwrap();
+        takes_as_ref(&month);
+        assert_eq!(month, month.clone());
+
+        bound!(FixedVec: Vec<bool> where |v| v.len() == 4);
+        let fixed = FixedVec::new(vec![false, true, false, false]).unwrap();
+        assert_eq!(fixed[1], true);
     }
 }
